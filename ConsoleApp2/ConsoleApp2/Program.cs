e1129d7693be6f7e914aec5145e0a391b3386828
Nasserxald <107666420+Nasserxald@users.noreply.github.com>
2022-06-25 18:56:27 +0300

    class work 2

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string HeroName = "Garelt";
+            int HeroPower = 100;
+            string VillainName = "wild hunt";
+            int VillainPower = 95;
+            if (HeroPower < VillainPower) 
+            {
+                Console.WriteLine(" The Villain is powerfull than the Hero ");
+            }
+            else if ( HeroPower == VillainPower )
+            {
+                Console.WriteLine(" None is powerfull "); 
+            }
+            else
+            {
+                Console.WriteLine(" The Hero is powerfull than the Villain ");
+            }
+        }
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            float playerSpeed = 0;
+
+            Console.WriteLine(playerSpeed);
+
+            void SetSpeed()
+            {
+                playerSpeed = 2.5F;
+            }
+            
+            Console.WriteLine(playerSpeed);
+            SetSpeed();
+            Console.WriteLine(playerSpeed);
+
+        }
+    }
+}
